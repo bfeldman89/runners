bfda1d5cd89de02c0dd45cac391683a13c1077bf
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2019-12-28 05:47:55 -0600

    Update pa_hourly_runs.sh

--- pa_hourly_runs.sh
+++ pa_hourly_runs.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-source ~/.bashrc
+
+# to run this script, the following cron job is set up:
+# 05 * * * * source ~/.bashrc && cd ~/code/runners && ./pa_hourly_runs.sh
 
 # RUN EVERY HOUR ON THE HOUR
-cd ~/code/jail_scrapers && python3 scrapers.py && python3 polish_data.py
-cd ~/code/mdoc_scraper && python3 mdoc_scraper.py
-cd ~/code/sos_scraper && python3 sos_scraper.py
-cd ~/code/reading_list && python3 muh_news.py
+python3 ~/code/jail_scrapers/scrapers.py
+python3 ~/code/jail_scrapers/polish_data.py
+python3 ~/code/mdoc_scraper/mdoc_scraper.py
+python3 ~/code/sos_scraper/sos_scraper.py
+python3 ~/code/reading_list/muh_news.py
 
 # RUN EVERY HOUR DEPENDING ON THE HOUR
 the_hour="$(date +"%H")"
 echo "Current hour : $the_hour"
 
 if ((9 <= $the_hour) && ($the_hour<= 19 )); then
-    echo "it's between 9am & 9pm"
-    cd ~/code/scheduled_tweets && python3 scheduled_tweets.py
+    python3 ~/code/scheduled_tweets/scheduled_tweets.py
 else
     echo "it's way too late/early for tweets"
 fi
 
 if [ $the_hour = "1" ]; then
     echo "bc it's 1am, umma check WaPo for fatal force updates"
-    cd ~/code/police_shootings && python3 police_shootings.py
+    python3 ~/code/police_shootings/police_shootings.py
 else
     echo "nah. I only check WaPo for fatal force updates at 1am"
 fi
