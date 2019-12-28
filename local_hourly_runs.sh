ca628fb496bbc35e197a79e74b0be1c0e255c43f
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2019-12-27 22:25:57 -0600

    Update local_hourly_runs.sh

--- local_hourly_runs.sh
+++ local_hourly_runs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 source ~/.bash_profile
 
 # work with pdfkit & imgkit stuff that doesn't work on pa
 cd ~/code/jail_scrapers && python3 pdf_stuff.py
 cd ~/code/msleg_scraper && python3 msleg_scraper.py
 
-# idk if permission settings can be created? So far, I've only run manually
+# TODO: find out how to automate deleting from reading list
 cd ~/code/reading_list && python3 get_reading_list.py
