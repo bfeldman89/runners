3994168c5f9e9cc2f6fabed46e9396131a7ea7d4
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2019-12-27 22:26:33 -0600

    Update pa_hourly_runs.sh

--- pa_hourly_runs.sh
+++ pa_hourly_runs.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 source ~/.bashrc
 
-the_hour="$(date +"%H")"
-echo "Current hour : $the_hour"
-
-# scrape the jails & run the cleanup script
-cd ~/code/jail_scrapers
-python3 scrapers.py && python3 polish_data.py
-
-# if/then commands relying on the hour of day
-if [ $the_hour = "21" ]; then
-    echo "bc it's 9-something-pm, umma check WaPo for fatal force updates"
-    cd ~/code/police_shootings && python3 police_shootings.py
-fi
-
-# TODO tweet if time w/in range
-#
-
-# run the other scrapers
-cd ~/code/scheduled_tweets && python3 scheduled_tweets.py
+# RUN EVERY HOUR ON THE HOUR
+cd ~/code/jail_scrapers && python3 scrapers.py && python3 polish_data.py
 cd ~/code/mdoc_scraper && python3 mdoc_scraper.py
 cd ~/code/sos_scraper && python3 sos_scraper.py
 cd ~/code/reading_list && python3 muh_news.py
+
+# RUN EVERY HOUR DEPENDING ON THE HOUR
+the_hour="$(date +"%H")"
+echo "Current hour : $the_hour"
+
+if (( 9 <= $the_hour <= 19 )); then
+    echo "it's between 9am & 9pm"
+    cd ~/code/scheduled_tweets && python3 scheduled_tweets.py
+else
+    echo "it's way too late/early for tweets"
+fi
+
+if [ $the_hour = "1" ]; then
+    echo "bc it's 1am, umma check WaPo for fatal force updates"
+    cd ~/code/police_shootings && python3 police_shootings.py
+else
+    echo "nah. I only check WaPo for fatal force updates at 1am"
+fi
