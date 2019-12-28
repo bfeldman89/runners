f68cda02d829f02fee88cead3d655f68cdc7b0a4
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2019-12-27 21:31:04 -0600

    Update local_hourly_runs.sh

--- local_hourly_runs.sh
+++ local_hourly_runs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 source ~/.bash_profile
+
+# work with pdfkit & imgkit stuff that doesn't work on pa
 cd ~/code/jail_scrapers && python3 pdf_stuff.py
 cd ~/code/msleg_scraper && python3 msleg_scraper.py
-cd ~/code/mdoc_scraper && python3 mdoc_scraper.py
-cd ~/code/sos_scraper && python3 sos_scraper.py
-cd ~/code/scheduled_tweets && python3 scheduled_tweets.py
-cd ~/code/reading_list && python3 muh_news.py
+
+# idk if permission settings can be created? So far, I've only run manually
+cd ~/code/reading_list && python3 get_reading_list.py
