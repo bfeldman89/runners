e539bd331b1b4ba00c735a22c43e8087f726bd59
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2019-12-28 18:18:35 -0600

    Update pa_hourly_runs.sh

--- pa_hourly_runs.sh
+++ pa_hourly_runs.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-# to run this script, the following cron job is set up:
-# 05 * * * * source ~/.bashrc && cd ~/code/runners && ./pa_hourly_runs.sh
-
-# RUN EVERY HOUR ON THE HOUR
+# 1. THE JAIL SCRAPING MODULES ARE RUN EVERY HOUR
 python3 ~/code/jail_scrapers/scrapers.py
 python3 ~/code/jail_scrapers/polish_data.py
-python3 ~/code/mdoc_scraper/mdoc_scraper.py
-python3 ~/code/sos_scraper/sos_scraper.py
-python3 ~/code/reading_list/muh_news.py
 
-# RUN EVERY HOUR DEPENDING ON THE HOUR
+# 2. A FEW SCRIPTS WILL ONLY RUN EVERY OTHER HOUR
+# a. Every odd hour, the SOS scraper runs
+# b. Every even hour, the MDOC scraper & a news scraper run
 the_hour="$(date +"%H")"
-echo "Current hour : $the_hour"
-
-if (( the_hour>8 && the_hour<20 )); then
-    python3 ~/code/scheduled_tweets/scheduled_tweets.py
+if (( "$[ $the_hour % 2 ]" == 1 )); then
+    python3 ~/code/sos_scraper/sos_scraper.py
 else
-    echo "it's way too late/early for tweets"
+    python3 ~/code/mdoc_scraper/mdoc_scraper.py
+    python3 ~/code/reading_list/muh_news.py
 fi
 
-if [ $the_hour = "1" ]; then
-    echo "bc it's 1am, umma check WaPo for fatal force updates"
+# 3. FINALLY, CERTAIN SCRIPTS ONLY RUN DURING TARGETTED HOURS OF THE DAY
+# a. Every hour, 9am - 7pm, if applicable, a scheduled tweet is tweeted.
+# b. Only once per day, at 1am, is WaPo's database checked for updates.
+if (( the_hour>8 && the_hour<20 )); then
+    python3 ~/code/scheduled_tweets/scheduled_tweets.py
+else if [ $the_hour = "1" ]; then
     python3 ~/code/police_shootings/police_shootings.py
-else
-    echo "nah. I only check WaPo for fatal force updates at 1am"
 fi
