4a621ac7f8b28e7b5fac6d7928797f18b79fa752
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-04-01 05:21:04 -0500

    pipenv shit

--- local_hourly_runs.sh
+++ local_hourly_runs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # work with pdfkit & imgkit stuff that doesn't work on pa
-cd ~/code/jail_scrapers && python3.6 pdf_stuff.py
-cd ~/code/msleg_scraper && python3.6 msleg_scraper.py && python3.6 senate_cmte_agendas.py
+cd ~/code/jail_scrapers && pipenv run python pdf_stuff.py
+cd ~/code/msleg_scraper && pipenv run python msleg_scraper.py && pipenv run python senate_cmte_agendas.py
 
 # TODO: find out how to automate deleting from reading list
 the_hour="$(date +"%H")"
 if [ "$the_hour" = "07" ]; then
-    cd ~/code/reading_list && python3.6 get_reading_list.py
+    cd ~/code/reading_list && pipenv run python get_reading_list.py
 fi
--- pa_hourly_runs.sh
+++ pa_hourly_runs.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # 1. THE JAIL SCRAPING MODULES ARE RUN EVERY HOUR
-python3 ~/code/jail_scrapers/scrapers.py
-python3 ~/code/jail_scrapers/polish_data.py
+pipenv run python ~/code/jail_scrapers/scrapers.py
+pipenv run python ~/code/jail_scrapers/polish_data.py
 
 # 2. A FEW SCRIPTS WILL ONLY RUN EVERY OTHER HOUR
 # a. Every odd hour, the SOS scraper runs
 # b. Every even hour, the MDOC scraper & a news scraper run
 the_hour="$(date +"%H")"
 if (( "$[ ${the_hour#0} % 2 ]" == 1 )); then
-    python3 ~/code/sos_scraper/sos_scraper.py
+    pipenv run python ~/code/sos_scraper/sos_scraper.py
 else
-    python3 ~/code/mdoc_scraper/mdoc_scraper_v2.py
-    python3 ~/code/reading_list/muh_news.py
+    pipenv run python ~/code/mdoc_scraper/mdoc_scraper_v2.py
+    pipenv run python ~/code/reading_list/muh_news.py
 fi
 
 # 3. FINALLY, CERTAIN SCRIPTS ONLY RUN DURING TARGETTED HOURS OF THE DAY
 # a. Every hour, 9am - 7pm, if applicable, a scheduled tweet is tweeted.
 # b. Only once per day, at 1am, is WaPo's database checked for updates.
 if (( ${the_hour#0}>7 && ${the_hour#0}<21 )); then
-    python3 ~/code/scheduled_tweets/scheduled_tweets.py
+    pipenv run python ~/code/scheduled_tweets/scheduled_tweets.py
 elif [ "$the_hour" = "07" ]; then
-    python3 ~/code/police_shootings/police_shootings.py
+    pipenv run python ~/code/police_shootings/police_shootings.py
 fi
