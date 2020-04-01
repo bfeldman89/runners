3e15237ceb980f2060d542c4f3753f95b0711885
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-04-01 09:49:12 -0500

    news scraping from local machine for a while

--- local_hourly_runs.sh
+++ local_hourly_runs.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # work with pdfkit & imgkit stuff that doesn't work on pa
-cd ~/code/jail_scrapers && pipenv run python pdf_stuff.py
-cd ~/code/msleg_scraper && pipenv run python msleg_scraper.py && pipenv run python senate_cmte_agendas.py
+cd ~/code/jail_scrapers
+pipenv run python pdf_stuff.py
+
+cd ~/code/msleg_scraper
+pipenv run python msleg_scraper.py
+pipenv run python senate_cmte_agendas.py
 
 # TODO: find out how to automate deleting from reading list
+cd ~/code/reading_list
 the_hour="$(date +"%H")"
-if [ "$the_hour" = "07" ]; then
-    cd ~/code/reading_list && pipenv run python get_reading_list.py
+if (( "$[ ${the_hour#0} % 2 ]" == 1 )); then
+    pipenv run python muh_news.py
+elif [ "$the_hour" = "05" ]; then
+    pipenv run python get_reading_list.py
 fi
