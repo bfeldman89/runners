fbf5538940f714500f1a533c048819f573a12bf8
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-07-18 22:17:04 -0500

    Update update_pa.sh

--- update_pa.sh
+++ update_pa.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+cd ~/code/3xp && git pull
+cd ~/code/jail_scrapers && git pull
 cd ~/code/mdoc_scraper && git pull
 cd ~/code/msleg_scraper && git pull
-cd ~/code/sos_scraper && git pull
-cd ~/code/jail_scrapers && git pull
-cd ~/code/reading_list && git pull
-cd ~/code/scheduled_tweets && git pull
 cd ~/code/mscourts_scraper && git pull
 cd ~/code/police_shootings && git pull
+cd ~/code/reading_list && git pull
 cd ~/code/runners && git pull
+cd ~/code/scheduled_tweets && git pull
+cd ~/code/sos_scraper && git pull
