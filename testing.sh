dfe514ac69e3cf96c27012f9f7256aa2dfd58096
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-08-28 01:36:37 -0500

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# working on PA (personal) consoles
+source ~/.bashrc && cd ~/code/jail_scrapers
+pipenv run python scrapers.py mcdc,prcdf,lcdc,jcadc 10
+pipenv run python scrapers.py jcj,acdc,hcdc 20
+
+# working on PA (splc account) consoles???
+source ~/.bashrc && cd ~/code/jail_scrapers && pipenv run python scrapers.py kcdc,jcdc,tcdc,ccdc,ccj 8
+
+# working on local Mac via crontab/terminal???
+source ~/.bashrc && cd ~/code/jail_scrapers && pipenv run python scrapers.py all
