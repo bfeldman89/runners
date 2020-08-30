7b128fcf0684dc96544d134d133217189bf49c5b
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-08-29 23:20:39 -0500

    Update pa_hourly_runs.sh

--- pa_hourly_runs.sh
+++ pa_hourly_runs.sh
@@ -4,6 +4,7 @@
 # Only 7 dockets are working via PyAn (mcdc, prcdf, lcdc, jcadc, hcdc, acdc, and jcj).
 # The other 5 dockets (kcdc, jcdc, tcdc, ccdc, and ccj) are scraped from the local netweork.
 # Because MS internet is trash, I scrape 2 dockets every odd hour and 3 dockets every even hour.
+
 cd ~/code/jail_scrapers
 pipenv run python scrapers.py mcdc,prcdf,lcdc,jcadc,hcdc,acdc,jcj 5
 pipenv run python polish_data.py
@@ -11,6 +12,7 @@ pipenv run python polish_data.py
 # 2. A FEW SCRIPTS WILL ONLY RUN EVERY OTHER HOUR
 # a. Every odd hour, the SOS scraper runs
 # b. Every even hour, the MDOC scraper & a news scraper run
+
 the_hour="$(date +"%H")"
 if (( $(( ${the_hour#0} % 2 )) == 1 )); then
     cd ~/code/sos_scraper && pipenv run python sos_scraper.py
@@ -22,6 +24,7 @@ fi
 # 3. FINALLY, CERTAIN SCRIPTS ONLY RUN DURING TARGETTED HOURS OF THE DAY
 # a. Every hour, 9am - 7pm, if applicable, a scheduled tweet is tweeted.
 # b. Only once per day, at 1am, is WaPo's database checked for updates.
+
 if (( ${the_hour#0}>7 && ${the_hour#0}<21 )); then
     cd ~/code/scheduled_tweets && pipenv run python scheduled_tweets.py
 fi
