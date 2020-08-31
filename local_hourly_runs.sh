8ba8f0c86ee6f09c89d82d9254de050a448d02b3
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-08-30 22:22:44 -0500

    Update local_hourly_runs.sh

--- local_hourly_runs.sh
+++ local_hourly_runs.sh
@@ -20,13 +20,15 @@ cd ~/code/msleg_scraper
 pipenv run python msleg_scraper.py
 pipenv run python senate_cmte_agendas.py
 
-# CHECK MDOC FOR COVID UPDATES EVERY HOUR
+# 3. CHECK MDOC FOR COVID UPDATES EVERY HOUR
 cd ~/code/mdoc_scraper && pipenv run python mdoc_covid.py
 
-# TODO: find out how to automate deleting from reading list
+# 4. SCRAPE NEWS AND CLEAN TEXT EVERY OTHER HOUR
 cd ~/code/reading_list
 if (( $(( ${the_hour#0} % 2 )) == 1 )); then
     pipenv run python muh_news.py
-elif [ "$the_hour" = "05" ]; then
+elif [ "$the_hour" == "22" ]; then
+    # once a day, the Safari reading list will be scraped
+    # this hour can be anytime, as long as it is even
     pipenv run python get_reading_list.py
 fi
