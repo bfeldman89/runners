c62e4776e440e2d4ce7467052c74819e28ae84bb
Blake Feldman <26087892+bfeldman89@users.noreply.github.com>
2023-09-27 18:52:26 -0500

    Update local_hourly_runs.sh

--- local_hourly_runs.sh
+++ local_hourly_runs.sh
@@ -4,16 +4,16 @@
 # Only 7 dockets are working via PyAn (mcdc, prcdf, lcdc, jcadc, hcdc, acdc, and jcj).
 # The other 5 dockets (kcdc, jcdc, tcdc, ccdc, and ccj) are scraped from the local netweork.
 # Because MS internet is trash, I scrape 2/5 dockets every odd hour and 3/5 dockets every even hour.
-cd ~/code/jail_scrapers
-the_hour="$(date +"%H")"
-if (( $(( ${the_hour#0} % 2 )) == 1 )); then
-    pipenv run python scrapers.py kcdc,ccj 7
-else
-    pipenv run python scrapers.py jcdc,tcdc,ccdc 7
-fi
+# cd ~/code/jail_scrapers
+# the_hour="$(date +"%H")"
+# if (( $(( ${the_hour#0} % 2 )) == 1 )); then
+#     pipenv run python scrapers.py kcdc,ccj 7
+# else
+#     pipenv run python scrapers.py jcdc,tcdc,ccdc 7
+# fi
 # clean the data and upload any new pdfs
-pipenv run python polish_data.py
-pipenv run python pdf_stuff.py
+# pipenv run python polish_data.py
+# pipenv run python pdf_stuff.py
 
 # 2. RUN THE MSLEG SCRAPERS LOCALLY EVERY HOUR
 cd ~/code/msleg_scraper
@@ -21,14 +21,14 @@ pipenv run python msleg_scraper.py
 pipenv run python senate_cmte_agendas.py
 
 # 3. CHECK MDOC FOR COVID UPDATES EVERY HOUR
-cd ~/code/mdoc_scraper && pipenv run python mdoc_covid.py
+# cd ~/code/mdoc_scraper && pipenv run python mdoc_covid.py
 
 # 4. SCRAPE NEWS AND CLEAN TEXT EVERY OTHER HOUR
-cd ~/code/reading_list
-if (( $(( ${the_hour#0} % 2 )) == 1 )); then
-    pipenv run python muh_news.py
-elif [ "$the_hour" == "22" ]; then
-    # once a day, the Safari reading list will be scraped
-    # this hour can be anytime, as long as it is even
-    pipenv run python get_reading_list.py
-fi
+# cd ~/code/reading_list
+# if (( $(( ${the_hour#0} % 2 )) == 1 )); then
+#     pipenv run python muh_news.py
+# elif [ "$the_hour" == "22" ]; then
+#     once a day, the Safari reading list will be scraped
+#     this hour can be anytime, as long as it is even
+#     pipenv run python get_reading_list.py
+# fi
